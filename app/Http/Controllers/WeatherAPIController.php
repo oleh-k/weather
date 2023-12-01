7f5c886d9f67726233f7d302e173d817c98a086e
oleh_ok <ok-ey@hotmail.com>
2023-12-01 19:27:02 +0200

    getWeather in controller

--- app/Http/Controllers/WeatherAPIController.php
+++ app/Http/Controllers/WeatherAPIController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\WeatherAPI;
+use Illuminate\Http\Request;
 
 class WeatherAPIController extends Controller
 {
-    public function getWeather() {
+    public function getWeather(Request $request) {
 
+        $weather = new WeatherAPI($request->all());
+        $response = $weather->getWeather();
+
+        if ($response["success"] === true) {
+            return $response;
+        } else {
+            return response($request, 400);
+        }
     }
 }
