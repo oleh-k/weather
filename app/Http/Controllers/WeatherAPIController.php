592343ff53e7f48c2ec5e86e5b3fee32fa8145bd
oleh_ok <ok-ey@hotmail.com>
2023-12-01 19:07:47 +0200

    route and WeatherAPIController

---
+++ app/Http/Controllers/WeatherAPIController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class WeatherAPIController extends Controller
+{
+    public function getWeather() {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\WeatherAPIController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,5 +11,6 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/weather', [WeatherAPIController::class, 'getWeather']);
 });
 
