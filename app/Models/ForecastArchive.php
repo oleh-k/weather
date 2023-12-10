b616275b0aacad9d1550a5ecd0126414fd14a90a
oleh_ok <ok-ey@hotmail.com>
2023-12-10 17:27:05 +0200

    add realation for ForecastArchive model forecast_archives -> cities forecast_archives -> forecast_data forecast_archives -> users

--- app/Models/ForecastArchive.php
+++ app/Models/ForecastArchive.php
@@ -23,4 +23,22 @@ class ForecastArchive extends Model
         'city_id',
         'user_id',
     ];
+
+    // Define a relationship with City
+    public function city()
+    {
+        return $this->belongsTo(City::class, 'city_id');
+    }
+
+    // Define the one-to-many relationship with ForecastData
+    public function forecastData()
+    {
+        return $this->hasMany(ForecastData::class, 'forecast_archive_id');
+    }
+
+    // Define the inverse of the one-to-many relationship with User
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
