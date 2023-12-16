3f657bf72929bcb12e7e1a476447118d7d3f7ad2
oleh_ok <ok-ey@hotmail.com>
2023-12-16 23:52:55 +0200

    test page

---
+++ resources/views/test.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test</title>
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
+</head>
+<body>
+<div id="app">
+    <example-component></example-component>
+</div>
+</body>
+</html>
