150940f3c1d28115f38a83afac166666acbbe4d6
oleh_ok <ok-ey@hotmail.com>
2023-12-18 23:17:13 +0200

    import element-plus to app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,16 @@
 
 import './bootstrap';
 import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
 
 const app = createApp({});
+app.use(ElementPlus);
 
 import ExampleComponent from './components/ExampleComponent.vue';
 app.component('example-component', ExampleComponent);
+
 
 import Register from './components/Register.vue';
 app.component('register', Register);
