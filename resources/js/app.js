f109a425105377251fadd55b9275a1b9190deef4
oleh_ok <ok-ey@hotmail.com>
2023-12-18 22:18:32 +0200

    create register component and import to app

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,5 +8,8 @@ const app = createApp({});
 import ExampleComponent from './components/ExampleComponent.vue';
 app.component('example-component', ExampleComponent);
 
+import Register from './components/Register.vue';
+app.component('register', Register);
+
 
 app.mount('#app');
---
+++ resources/js/components/Register.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">register</div>
+
+                    <div class="card-body">
+                        {{ input }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            input: 'register'
+        }
+    },
+    mounted() {
+        console.log('Component mounted.')
+    }
+}
+</script>
