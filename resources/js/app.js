f8805c241a86c98ca990714f3d6e82b50a4601a5
oleh_ok <ok-ey@hotmail.com>
2023-12-18 23:20:20 +0200

    removed test component

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,9 +8,6 @@ import 'element-plus/dist/index.css';
 const app = createApp({});
 app.use(ElementPlus);
 
-import ExampleComponent from './components/ExampleComponent.vue';
-app.component('example-component', ExampleComponent);
-
 
 import Register from './components/Register.vue';
 app.component('register', Register);
